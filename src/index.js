303f5de4122046a824941ebf640a523cbe2032c6
Niklas Kaulfers <niklaskaulfers2@gmail.com>
2024-10-25 09:23:20 +0200

    index wasnt loading properly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// whattafak
+
 document.getElementById('send-btn').addEventListener('click', function() {
     const chatBox = document.getElementById('chat-box');
     const chatInput = document.getElementById('chat-input');
