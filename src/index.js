29f725206a494d5055a78aef66eb77a95e4c5926
Niklas Kaulfers <niklaskaulfers2@gmail.com>
2024-11-07 15:20:28 +0100

    hopeful fixes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 // whattafak
 
-document.getElementById('send-btn').addEventListener('click', sendMessage());
+document.getElementById('send-btn').addEventListener('click', sendMessage(document.getElementById("chat-box"), document.getElementById("chat-input")));
 document.getElementById('chat-input').addEventListener("keypress", function(e){
     if (e.key === "Enter"){
-        sendMessage();
+        sendMessage(document.getElementById("chat-box"), document.getElementById("chat-input"));
     }
 });
 
-function sendMessage(){
-    const chatBox = document.getElementById('chat-box');
-    const chatInput = document.getElementById('chat-input');
+function sendMessage(chatBox, chatInput){
     const message = chatInput.value;
 
     if (message.trim() !== '') {
