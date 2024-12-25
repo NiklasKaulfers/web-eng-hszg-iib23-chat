b74af3e09005286146a08298d52e87f00e6843dd
Niklas Kaulfers <niklaskaulfers2@gmail.com>
2024-12-25 13:50:48 +0100

    ws impl

---
+++ src/joinRoom.js
@@ -0,0 +1,38 @@
+const socket = new WebSocket('https://web-ing-iib23-chat-app-backend-377dbfe5320c.herokuapp.com/');
+
+socket.onopen = () => {
+    console.log('Connected to WebSocket server');
+    socket.send('Hello from GitHub Pages!');
+};
+
+socket.onmessage = (event) => {
+    console.log('Message from server:', event.data);
+};
+
+socket.onclose = () => {
+    console.log('Disconnected from WebSocket server');
+};
+
+socket.onerror = (error) => {
+    console.error('WebSocket error:', error);
+};
+document.getElementById('send-btn')
+    .addEventListener('click', sendMessage(document.getElementById("chat-box")))
+document.getElementById('chat-input').addEventListener("keypress", function(e){
+    if (e.key === "Enter"){
+        sendMessage(document.getElementById("chat-box"), document.getElementById("chat-input"));
+    }
+});
+
+function sendMessage(chatBox, chatInput){
+    const message = chatInput.value;
+
+    if (message.trim() !== '') {
+        socket.send(message)
+        const messageElement = document.createElement('div');
+        messageElement.textContent = message;
+        chatBox.appendChild(messageElement);
+        chatInput.value = '';
+        chatBox.scrollTop = chatBox.scrollHeight;
+    }
+}
