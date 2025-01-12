7861f67fc258c76919974737e414a151fe3935ba
NiklasKaulfers <niklas.kaulfers@zeiss.com>
2025-01-12 17:17:59 +0100

    changed to node from php

---
+++ index.js
@@ -0,0 +1,16 @@
+import Express from 'express';
+import * as http from 'http';
+import * as path from 'path';
+
+const app = Express();
+const server = http.createServer(app);
+const port = process.env.PORT || 8080;
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
