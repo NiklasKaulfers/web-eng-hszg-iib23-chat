df8192c1c7fb5e1236fb90a3181aa86cc7268200
NiklasKaulfers <niklas.kaulfers@zeiss.com>
2025-01-12 17:41:01 +0100

    fix for common js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import Express from 'express';
 import * as http from 'http';
 import * as path from 'path';
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = Express();
 const server = http.createServer(app);
